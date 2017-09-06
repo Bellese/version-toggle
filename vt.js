d3262590791f27d6383f4be97b94ddcf1cf8b3b6
Josh Offutt IV <joffutt@bellesetech.com>
2017-09-06 16:49:28 -0400

    Added list parameter
    Now allows a list of conditions to be passed in. Still allows single conditions as well and combines all singles and the list passed into conditions for the version toggling.

--- vt.js
+++ vt.js
@@ -16,13 +16,25 @@ function collectConditions(con, conditions) {
     return conditions;
 }
 
+function list(list) {
+    var conditions = [],
+        split = list.split(',');
+    for (var i = 0; i < split.length; i++) {
+        var conditionSplit = split[i].split(":"),
+            condition = JSON.parse("{\"" + conditionSplit[0] + "\":\"" + conditionSplit[1] + "\"}");
+        conditions.push(condition);
+    }
+    return conditions;
+}
+
 //Add the parameters to the application
 program
     .version('2.0.0')
     .usage('<options>')
     .option('-i, --inputDir [inputDir]', 'Sets the input directory, defaults to src/', 'src/')
     .option('-o, --outputDir [outputDir]', 'Sets the output directory, defaults to ver/', 'ver/')
-    .option('-c, --conditions <conditions>', 'Sets the conditions for version toggling. Can be called multiple times to add in multiple conditions. Condition object structure: featureName:sem.ver.sion', collectConditions, [])
+    .option('-c, --conditions [conditions]', 'Adds a condition for version toggling. Can be called multiple times to add in multiple conditions. Condition object structure: featureName:sem.ver.sion', collectConditions, [])
+    .option('-l, --list [list]', 'Adds a list of conditions for version toggling. List object structure: featureName:sem.ver.sion,another:sem.ver.sion', list, [])
     .option('-e, --exact [exact]', 'Set to false to allow loose matching, defaults to true (exact matching)', true)
     .parse(process.argv);
 
@@ -31,7 +43,7 @@ var options = {
     inputDir: program.inputDir,
     outputDir: program.outputDir,
     exact: program.exact,
-    conditions: program.conditions
+    conditions: program.conditions.concat(program.list)
 }
 
 versionToggle(options);
